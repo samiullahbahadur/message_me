dbb1a693e1538574de26f18f02b55ced66568a9d
samiullahbhadur <bahadursamiullah@gmail.com>
2022-07-18 00:10:15 +0430

    Add navbar

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,17 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+//= require rails-ujs
+//= require jquery
+//= require activestorage
+//= require semantic-ui
+//= require turboLinks
+//= require_tree
+
+
+
+
+$(document).on('turboLinks:load', function(){
+    $('.ui.dropdown').dropdown();
+  
+})
