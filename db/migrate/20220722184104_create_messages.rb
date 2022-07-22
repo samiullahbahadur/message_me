1f1d8c156c65d39006c390a9d5e2f366c6623d4d
samiullahbhadur <bahadursamiullah@gmail.com>
2022-07-22 23:18:54 +0430

    Add message controller

--- db/migrate/20220722184104_create_messages.rb
+++ db/migrate/20220722184104_create_messages.rb
@@ -1,8 +1,8 @@
 class CreateMessages < ActiveRecord::Migration[7.0]
   def change
     create_table :messages do |t|
-      t.text :body
-      t.references :users, null: false, foreign_key: true
+        t.text :body
+        t.integer :user_id
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_22_181217) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_22_184104) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "messages", force: :cascade do |t|
     t.text "body"
-    t.bigint "users_id", null: false
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["users_id"], name: "index_messages_on_users_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -29,5 +28,4 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_22_181217) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "messages", "users", column: "users_id"
 end
