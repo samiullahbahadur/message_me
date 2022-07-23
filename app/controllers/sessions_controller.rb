052fc5998ea232654542fef8bdf9f9f1f55a17e0
samiullahbhadur <bahadursamiullah@gmail.com>
2022-07-23 22:39:19 +0430

    Add authentication part

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,17 +7,17 @@ class SessionsController < ApplicationController
         if user.present? && user.authenticate(params[:password])
         #good scenario
         session[:user_id]=user.id 
-        flash[:success] ='You have Successfully Logged in'
+        flash[:notice] ='You have Successfully Logged in'
         redirect_to root_path
        else
-        flash.now[:error]= 'there was somthing wrong'
+        flash.now[:alert]= 'invalid  authentication  username or password'
         render 'new'
        end
     end
 
     def destroy 
         session[:user_id]=nil
-        flash[:success]="You have successfully Logged out"
+        flash[:notice]="You have successfully Logged out"
         redirect_to login_path
     end
     
