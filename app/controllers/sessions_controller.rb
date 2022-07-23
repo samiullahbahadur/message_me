9553066b8c572cc5b5729995c68b69b0d05a3223
samiullahbhadur <bahadursamiullah@gmail.com>
2022-07-23 21:30:50 +0430

    Fix login and logou path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,14 +3,14 @@ class SessionsController < ApplicationController
         @user=User.new  
     end
     def create 
-        user=User.find_by(username: params[:session][:username])
-       if user && user.authenticate(params[:session][:password])
+        user = User.find_by(username: params[:username])
+        if user.present? && user.authenticate(params[:password])
         #good scenario
-        session[:user_id]=user.user.id 
+        session[:user_id]=user.id 
         flash[:success] ='You have Successfully Logged in'
         redirect_to root_path
        else
-        flash.now[:error] 'there was somthing wrong'
+        flash.now[:error]= 'there was somthing wrong'
         render 'new'
        end
     end
@@ -19,5 +19,6 @@ class SessionsController < ApplicationController
         session[:user_id]=nil
         flash[:success]="You have successfully Logged out"
         redirect_to login_path
+    end
     
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
-  post 'message', to: 'messages#create'
+  post 'message', to: 'messages#create '
 
   mount ActionCable.server, at: '/cable'
 end
